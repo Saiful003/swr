4805e0628a6496de4b45c12cf78e5fc25b137a6e
Md Saiful Islam Shanto <mdsaifulislamshanto03@gmail.com>
2022-07-02 05:23:25 -0700

    Overlay issue solved

--- components/OverLay.tsx
+++ components/OverLay.tsx
@@ -6,7 +6,7 @@ function OverLay({ closeDrawer, isDrawerOpen }: props) {
   return (
     <div
       onClick={closeDrawer}
-      className={`fixed inset-0 bg-white opacity-0 pointer-events-none md:opacity-0  ${
+      className={`fixed  cursor-pointer inset-0 bg-white opacity-0 pointer-events-none md:opacity-0  ${
         isDrawerOpen && "  opacity-40 pointer-events-auto"
       }`}
     ></div>
