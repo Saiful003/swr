b0f59fb550b5eed89e237d67d632e8a186de221a
Md Saiful Islam Shanto <mdsaifulislamshanto03@gmail.com>
2022-07-02 05:30:23 -0700

    Logo size increase

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
         <div className=" w-[90%] max-w-6xl mx-auto px-5">
           <div className="h-[150px] flex items-center justify-between">
             <div>
-              <h2 className=" text-2xl text-white">Logo</h2>
+              <h2 className=" text-4xl text-white">Logo</h2>
             </div>
             <div className="hidden md:block">
               <ul className="flex gap-5">
