69906c4c9c62c30866caf1c8f9e098e619a048c4
Md Saiful Islam Shanto <mdsaifulislamshanto03@gmail.com>
2022-10-05 14:27:38 -0600

    add domain name

--- config/axios.js
+++ config/axios.js
@@ -1,3 +1,5 @@
 import axios from "axios";
-const customAxios = axios.create({ baseURL: "http://localhost:3000/api/v1" });
+const customAxios = axios.create({
+  baseURL: "https://friends-management.vercel.app/api/v1",
+});
 export default customAxios;
