d68c2b1ddb98777e16d137dde6aa6f5606020309
Md Saiful Islam Shanto <mdsaifulislamshanto03@gmail.com>
2022-10-06 02:49:52 -0600

    Hope this time would be work

--- config/axios.js
+++ config/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const customAxios = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://friends-management.vercel.app/api/v1",
 });
 export default customAxios;
