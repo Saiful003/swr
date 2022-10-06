b2e802e1c2befac12c2db7d06695db64b5d4bfcd
Md Saiful Islam Shanto <mdsaifulislamshanto03@gmail.com>
2022-10-06 03:06:13 -0600

    Try solve by changed cloudinary configuration

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -4,8 +4,8 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
-  shorten: true,
-  secure: true,
+  // shorten: true,
+  // secure: true,
   // ssl_detected: true,
 });
 
