d89a58b245023f83d8e1067c9176098d744d851d
Md Saiful Islam Shanto <mdsaifulislamshanto03@gmail.com>
2022-11-21 00:53:08 -0600

    Ok

--- hooks/usePost.js
+++ hooks/usePost.js
@@ -3,6 +3,7 @@ import { fetcher } from "../utils/fetcher";
 
 export function usePost() {
   const { data, error } = useSWR("/api/v1/", fetcher);
+  if (!data) return;
 
   return {
     posts: data.message,
