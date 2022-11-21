a7f9ed507e6624ffe76c63c5ab8537ee62ceee54
Md Saiful Islam Shanto <mdsaifulislamshanto03@gmail.com>
2022-11-21 00:50:55 -0600

    Ok

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -7,27 +7,28 @@ import useSWR, { SWRConfig, useSWRConfig, unstable_serialize } from "swr";
 import { fetcher } from "../../utils/fetcher";
 
 // Generates `/posts/1` and `/posts/2`
-export async function getStaticPaths() {
-  const { data } = await customAxios.get("/");
-  const paths = data.message.map((post) => ({ params: { id: post._id } }));
-  return {
-    paths,
-    fallback: true, // can also be true or 'blocking'
-  };
-}
-export async function getStaticProps({ params: { id } }) {
-  // fetch data`/api/v1/post/${id}`
-  const { data } = await customAxios.get(`/post/${id}`);
-  return {
-    props: {
-      fallback: {
-        [unstable_serialize(["api", "v1", "post", id])]: data,
-      },
-    },
-  };
-}
+// export async function getStaticPaths() {
+//   const { data } = await customAxios.get("/");
+//   console.log(data);
+//   const paths = data.message.map((post) => ({ params: { id: post._id } }));
+//   return {
+//     paths: [],
+//     fallback: true, // can also be true or 'blocking'
+//   };
+// }
+// export async function getStaticProps({ params: { id } }) {
+//   // fetch data`/api/v1/post/${id}`
+//   const { data } = await customAxios.get(`/post/${id}`);
+//   return {
+//     props: {
+//       fallback: {
+//         [unstable_serialize(["api", "v1", "post", id])]: data,
+//       },
+//     },
+//   };
+// }
 
-function Post() {
+export default function Post() {
   const [comment, setComment] = useState("");
   const { data: session } = useSession();
   const { mutate } = useSWRConfig();
@@ -35,7 +36,8 @@ function Post() {
   const {
     query: { id },
   } = useRouter();
-  const { data } = useSWR(["api", "v1", "post", id], fetcher);
+  const { data } = useSWR(`/api/v1/post/${id}`, fetcher);
+  if (!data) return;
   const {
     message: { comments, ...post },
   } = data;
@@ -73,10 +75,10 @@ function Post() {
   );
 }
 
-export default function PostWrapper({ fallback }) {
-  return (
-    <SWRConfig value={{ fallback }}>
-      <Post />
-    </SWRConfig>
-  );
-}
+// export default function PostWrapper({ fallback }) {
+//   return (
+//     <SWRConfig value={{ fallback }}>
+//       <Post />
+//     </SWRConfig>
+//   );
+// }
